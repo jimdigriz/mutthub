8e23f579f31b341bfa46af4852be618ac11720e7
Alexander Clouter <alex@digriz.org.uk>
2025-10-29 22:10:26 +0000

    pre-process the message to be replied to

--- editor.sh
+++ editor.sh
@@ -4,11 +4,6 @@ set -eu
 
 DIR=$(dirname $(readlink -f "$0"))
 
-cleanup() {
-	rm -f "$1.tmp"
-}
-trap cleanup EXIT
-
 while getopts f:p F; do
 	case $F in
 	f)	FARG=$OPTARG;;
@@ -17,25 +12,46 @@ while getopts f:p F; do
 done
 shift $((OPTIND - 1))
 
+M=$1
+
+cleanup() {
+	rm -f "$M.orig" "$M.tmp"
+}
+trap cleanup EXIT
+
+cp -p "$M" "$M.orig"
+
+# we need to reverse mutt adding an attribution and indent strings
+A=$(head -n1 "$M")
+sed -e '1 d; s/^> //' "$M" > "$M.tmp"
+mv "$M.tmp" "$M"
+
+# preserve the signature seperately
+S=$(tail -n2 "$M")
+head -n -2 "$M" > "$M.tmp"
+mv "$M.tmp" "$M"
+
 # not passthrough
 if [ ! "${PARG:-}" ]; then
-	# we need to reverse mutt adding an attribution and indent strings
-	A=$(head -n1 "$1")
-	sed -e '1 d; s/^> //;' "$1" > "$1.tmp"
-	mv "$1.tmp" "$1"
-
-	# preserve the signature seperately
-	S=$(tail -n2 "$1")
-	head -n -2 "$1" > "$1.tmp"
-	mv "$1.tmp" "$1"
-
-	/bin/sh "$DIR/format-email.sh" ${FARG:+-f $FARG} < "$1" > "$1.tmp"
-	mv "$1.tmp" "$1"
-
-	# ...and now add back in the attribution, signature and indent strings
-	{ echo "$A"; sed -e 's/^/> /;' "$1"; echo "$S"; } > "$1.tmp"
-	mv "$1.tmp" "$1"
+	/bin/sh "$DIR/format-email.sh" ${FARG:+-f $FARG} < "$M" > "$M.tmp"
+	mv "$M.tmp" "$M"
 fi
 
+sed -e '/^> > On .* wrote:$/ d; /^> > > / d' "$M" \
+	| sed -e '$ { /^> > / d }' \
+	> "$M.tmp"
+mv "$M.tmp" "$M"
+
+# ...and now add back in the attribution, signature and indent strings
+{ printf '\n%s\n' "$A"; sed -e 's/^/> /' "$M"; printf '\n%s' "$S"; } > "$M.tmp"
+mv "$M.tmp" "$M"
+
+CHKSUM=$(openssl dgst -md5 -r "$M" | awk '{ print $M }')
+
 # https://rinzewind.org/blog-en/2017/a-small-trick-for-sending-flowed-mail-in-mutt-with-vim.html
-exec vim -c 'setlocal fo=awnqptc comments=nb:> filetype=mail wm=0 tw=72 nonumber digraph list nojs nosmartindent spell spelllang=en_gb' "$1"
+vim -c 'setlocal fo=awnqptc comments=nb:> filetype=mail wm=0 tw=72 nonumber digraph list nojs nosmartindent spell spelllang=en_gb' "$M"
+
+# allow 'abort_unmodified' to work
+[ "$(openssl dgst -md5 -r "$M" | awk '{ print $M }')" != "$CHKSUM" ] || mv "$M.orig" "$M"
+
+exit 0
